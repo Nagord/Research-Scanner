a55f9f50e8001b2423c556937dfc20c511603d0e
DragonFire47 <1redyoung@gmail.com>
2020-08-08 16:51:18 -0400

    Add project files.

---
+++ Research Scanner/Patch.cs
@@ -0,0 +1,31 @@
+﻿using CodeStage.AntiCheat.ObscuredTypes;
+using HarmonyLib;
+using PulsarPluginLoader.Utilities;
+using System.Collections.Generic;
+
+namespace Research_Scanner
+{
+    [HarmonyPatch(typeof(PLServer), "NetworkedStartEnhancedEMScan")]
+    class Patch
+    {
+        static void Postfix(ref int shipID)
+        {
+            if (shipID == PLEncounterManager.Instance.PlayerShip.ShipID && PhotonNetwork.isMasterClient)
+            {
+                bool hasfoundresearch = false;
+                foreach (KeyValuePair<ObscuredInt, ObscuredBool> MyResearch in PLEncounterManager.Instance.GetCurrentPersistantEncounterInstance().MyPersistantData.ProbePickupPersistantData)
+                {
+                    if (MyResearch.Value)
+                    {
+                        hasfoundresearch = true;
+                        break;
+                    }
+                }
+                if (hasfoundresearch)
+                {
+                    Messaging.Notification("Scanner has found research!", PhotonTargets.All);
+                }
+            }
+        }
+    }
+}
---
+++ Research Scanner/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace Research_Scanner
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "0.0.1";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "ResearchScanner";
+
+        public override int MPFunctionality => (int)MPFunction.HostOnly;
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.ResearchScanner";
+        }
+    }
+}
