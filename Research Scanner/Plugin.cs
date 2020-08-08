ee025d0b769fde672f9699382c687092d268cc85
DragonFire47 <1redyoung@gmail.com>
2020-08-08 16:52:07 -0400

    Incriment version

--- Research Scanner/Plugin.cs
+++ Research Scanner/Plugin.cs
@@ -4,7 +4,7 @@ namespace Research_Scanner
 {
     public class Plugin : PulsarPlugin
     {
-        public override string Version => "0.0.1";
+        public override string Version => "0.0.2";
 
         public override string Author => "Dragon";
 
